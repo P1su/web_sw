8c3a97e6137ddfdd9db0fea29dc5b3ae4f94067d
P1su <pissssssu@naver.com>
2024-11-14 00:08:39 +0900

    feat: Header 컴포넌트 생성

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import styles from './Header.module.css';
+import logo from '../../assets/svg/logo.svg';
+const Header = () => {
+  const navItems = [
+    { label: '후기', path: '/' },
+    { label: '자재', path: '/about' },
+    { label: '문의', path: '/contact' },
+  ];
+  return(
+    <header className={styles.header}>
+      <img src={logo}/>
+      <nav className={styles.navBar}>
+        {
+          navItems.map((item) => (
+            <a key={item.label} className={styles.navItems}>
+              {item.label}
+            </a>
+          ))
+        }
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,10 @@
+import Header from '../../components/Header/Header';
 import styles from './Main.module.css'
 
 const Main = () => {
   return(
     <div className={styles.test}>
-      <h1>Main 페이지</h1>
+      <Header />
     </div>
   );
 };
