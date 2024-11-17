e553029609a5b55380e36c8f348c835ff5971ee4
P1su <pissssssu@naver.com>
2024-11-18 01:07:15 +0900

    feat: Input 컴포넌트 생성

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,8 @@
+import BtnPrimary from '../../components/buttons/BtnPrimary/BtnPrimary';
 import BtnLarge from '../../components/buttons/Large/BtnLarge';
 import styles from './Main.module.css'
+import BtnSmall from './../../components/buttons/Small/BtnSmall';
+import Input from '../../components/forms/input/Input';
 
 const Main = () => {
   return(
@@ -7,6 +10,9 @@ const Main = () => {
       메인페이지
       <BtnLarge isFilled={false}>큰 버튼</BtnLarge>
       <BtnLarge isFilled={true}>큰 버튼</BtnLarge>
+      <BtnSmall isFilled={false}>작은 버튼</BtnSmall>
+      <BtnPrimary isFilled={false}>Primary 버튼</BtnPrimary>
+      <Input title='성명' placeholder='성명'/> 
     </div>
   );
 };
