170093286d6c5b3a28f097c6c4b6d8e638eea436
P1su <pissssssu@naver.com>
2024-11-21 23:33:01 +0900

    feat: 버튼 이벤트 구현

--- src/pages/Materials/MaterialDetail/MaterialDetail.jsx
+++ src/pages/Materials/MaterialDetail/MaterialDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import mockImg from '../../../assets/img/mockItem.png';
 import styles from './MaterialDetail.module.css';
 import Title from '../../../components/Title/Title';
@@ -6,6 +6,11 @@ import BtnSmall from '../../../components/buttons/Small/BtnSmall';
 
 const MaterialDetail = () => {
   const { name } = useParams();
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
   console.log(name);
   //name 통해서 get이나 포스트로 받아오기
 
@@ -18,8 +23,8 @@ const MaterialDetail = () => {
       <p className={styles.infoTextSpan}>{`패턴: ${item.pattern}`}</p>
       <p className={styles.infoTextSpan}>{`코팅: ${item.coating}`}</p>
       <div className={styles.buttonField}>
-        <BtnSmall isFilled={false}>목록으로 돌아가기</BtnSmall>
-        <BtnSmall isFilled={false}>자재 비교하기</BtnSmall>
+        <BtnSmall onClick={() => handleNavigate('/materials')} isFilled={false}>목록으로 돌아가기</BtnSmall>
+        <BtnSmall onClick={() => handleNavigate('/materials')} isFilled={false}>자재 비교하기</BtnSmall>
       </div>
     </div>
   );
