0a4dee0eecde234241c46d16b4660bac67673163
P1su <pissssssu@naver.com>
2024-11-28 15:20:32 +0900

    style: 전체 padding 삭제

--- src/components/forms/textarea/Textarea.jsx
+++ src/components/forms/textarea/Textarea.jsx
@@ -2,10 +2,10 @@ import styles from './Textarea.module.css';
 
 const Textarea = ({ placeholder, title }) => {
   return(
-    <>
+    <div className={styles.textareaWrapper}>
       <p className={styles.titleText}>{title}</p>
       <textarea className={styles.textarea} placeholder={placeholder}/>
-    </>
+    </div>
   );
 };
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,7 +4,6 @@ import styles from './Main.module.css'
 import BtnSmall from './../../components/buttons/Small/BtnSmall';
 import Input from '../../components/forms/input/Input';
 import Search from '../../components/forms/search/Search';
-import Textarea from '../../components/forms/textarea/Textarea';
 import MaterialItem from '../../components/Material/MaterialItem/MaterialItem';
 import MaterialList from '../../components/Material/MaterialList/MaterialList';
 import Title from '../../components/Title/Title';
@@ -12,7 +11,7 @@ import MyCalendar from '../../components/Calendar/MyCalendar';
 
 const Main = () => {
   return(
-    <div className={styles.test}>
+    <div className={styles.mainWrapper}>
       <Title>메인페이지</Title>
       <BtnLarge isFilled={false}>큰 버튼</BtnLarge>
       <BtnLarge isFilled={true}>큰 버튼</BtnLarge>
@@ -20,7 +19,6 @@ const Main = () => {
       <BtnPrimary isFilled={false}>Primary 버튼</BtnPrimary>
       <Input title='성명' placeholder='성명'/> 
       <Search placeholder='원하시는 자재를 입력하세요'/>
-      <Textarea placeholder='자재 설명 입력' title='자재'/>
       <MaterialItem />
       <MaterialList />
       <MyCalendar />
