4ff0b1142acc7474c0350fffd1ab883b48e10c66
P1su <pissssssu@naver.com>
2024-11-28 18:46:14 +0900

    style: 헤더 스타일 변경

---
+++ src/utils/url.js
@@ -0,0 +1,3 @@
+export const blogUrl = 'https://blog.naver.com/prologue/PrologueList.naver?blogId=kws9886&skinType=&skinId=&from=menu&userSelectMenu=true';
+export const instaUrl = 'https://www.instagram.com/e.ggeulrim/#';
+export const youtubeUrl = 'https://www.youtube.com/channel/UCm_t3h3Ji63G_5b9Xe9SBFA';
