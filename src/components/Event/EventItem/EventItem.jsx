2d61006357f57ce8fd5f72a794d9fc5d0d36bea4
P1su <pissssssu@naver.com>
2024-12-13 14:20:43 +0900

    feat: EventItem 컴포넌트 생성

--- src/components/Event/EventItem/EventItem.jsx
+++ src/components/Event/EventItem/EventItem.jsx
@@ -3,7 +3,9 @@ import styles from './EventItem.module.css';
 const EventItem = () => {
   return (
     <div className={styles.eventItemWrapper}>
-      
+      <img className={styles.img}/>
+      <p className={styles.title}>12월 이벤트</p>
+      <span>2024.12.31</span>
     </div>
   );
 };
--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,10 +1,19 @@
+import EventItem from '../../components/Event/EventItem/EventItem';
 import Title from '../../components/Title/Title';
 import styles from './Event.module.css';
 
 const Event = () => {
   return(
     <div className={styles.pageWrapper}>
-      <Title>이달의 이벤트</Title>
+      <Title>Events</Title>
+      <section>
+        <h2 className={styles.titleText}>이달의 이벤트</h2>
+        <p className={styles.text}>
+          매달 진행되는 새로운 이벤트들을 확인해보세요<br />
+        </p>
+        <hr className={styles.hr}/>
+      </section>
+      <EventItem />
     </div>
   );
 };
