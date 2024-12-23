1e439035655720045639ca1983c8b33227389587
P1su <pissssssu@naver.com>
2024-12-24 00:00:23 +0900

    refactor: Suspense 활용한 lazy loading 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import router from './routes/Router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Suspense } from 'react';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      </Suspense>
     </QueryClientProvider>
   )
 }
